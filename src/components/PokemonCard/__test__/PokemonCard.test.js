cd8b9afe1e34fcf1eb43d2c8e364404dbed8899c
Matheus Neves <matheusneves1989@gmail.com>
2021-08-26 20:09:34 -0300

    feat: PokemonCard component

--- src/components/PokemonCard/__test__/PokemonCard.test.js
+++ src/components/PokemonCard/__test__/PokemonCard.test.js
@@ -1,1 +1,37 @@
-// TODO testar component
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react'
+import PokemonCard from '../PokemonCard'
+import { cardBuilder } from '../../../__mocks__/card-builder'
+
+const setup = (props = {
+  card: cardBuilder({ name: 'Meu Pokemon' })
+}) => {
+  const { card } = props;
+  const onClick = jest.fn();
+  const imageAlt = `${card.id}-${card.name}`;
+  const renderResult = render(<PokemonCard {...card} onClick={onClick} />)
+  const imageEl = renderResult.getByAltText(`${card.id}-${card.name}`)
+  return {
+    card,
+    imageEl,
+    imageAlt,
+    onClick,
+    ...renderResult,
+  };
+}
+
+describe('PokemonCard', () => {
+  it('should be render with default props', () => {
+    const { card, imageAlt, imageEl } = setup();
+
+    expect(imageEl).toBeInTheDocument();
+    expect(imageEl.src).toBe(card.imageUrl);
+    expect(imageEl.alt).toBe(imageAlt);
+  })
+
+  it('should emit event onClick', () => {
+    const { imageEl, onClick } = setup()
+    fireEvent.click(imageEl);
+    expect(onClick).toHaveBeenCalledTimes(1);
+  })
+})
